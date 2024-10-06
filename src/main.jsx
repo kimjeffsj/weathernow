888f23ba0d3adb326959b82bf6a6d2528cfc19c8
kimjeffsj <kim.jeffsj@gmail.com>
2024-10-06 16:40:10 -0700

    Basic Login/Register with Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import App from "./app/App";
+import Login from "./pages/login";
+import Register from "./pages/Register";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
---
+++ src/pages/Register.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../shared/api/firebase";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [error, setError] = useState(null);
+
+  const resetForm = () => {
+    setEmail("");
+    setPassword("");
+    setPassword2("");
+    setError(null);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    if (password !== password2) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      // const userCredential = await createUserWithEmailAndPassword(
+      //   auth,
+      //   email,
+      //   password
+      // );
+      // console.log("Register success", userCredential);
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert(`Register success, please login`);
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        setError("Email already in use");
+        resetForm();
+      } else {
+        setError("Failed to register: " + error.message);
+      }
+      // console.error("Failed to register", error.message);
+      resetForm();
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+          required
+        />
+        <input
+          type="password"
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+          placeholder="confirm password"
+          required
+        />
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
