e0020b3749b287e992581f15f71527d45654fa8f
kimjeffsj <kim.jeffsj@gmail.com>
2024-10-13 18:04:05 -0700

    overall styling, login/register delted, firebase deleted

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
 import DetailedWeather from "../pages/DetailedWeather";
+
+import "./styles/index.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to Weather Now</h1>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />}></Route>
         <Route path="/weather/:city" element={<DetailedWeather />} />
       </Routes>
     </div>
--- src/shared/api/weather.js
+++ src/shared/api/weather.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
-const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+const BASE_URL = "https://api.openweathermap.org/";
 
 const getCurrentWeather = async (city) => {
   try {
-    const response = await axios.get(`${BASE_URL}/weather`, {
+    const response = await axios.get(`${BASE_URL}/data/2.5/weather`, {
       params: {
         q: city,
         appid: API_KEY,
         units: "metric",
       },
     });
-    console.log(response);
+    // console.log(response);
     return response.data;
   } catch (error) {
     console.error("Error fetching weather data: " + error);
@@ -20,9 +20,26 @@ const getCurrentWeather = async (city) => {
   }
 };
 
+const getCurrentWeatherByCoords = async (lat, lon) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/data/2.5/weather`, {
+      params: {
+        lat,
+        lon,
+        appid: API_KEY,
+        units: "metric",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching weather data by coordinates: " + error);
+    throw error;
+  }
+};
+
 const getForecast = async (city) => {
   try {
-    const response = await axios.get(`${BASE_URL}/forecast`, {
+    const response = await axios.get(`${BASE_URL}/data/2.5/forecast`, {
       params: {
         q: city,
         appid: API_KEY,
@@ -36,4 +53,27 @@ const getForecast = async (city) => {
   }
 };
 
-export { getCurrentWeather, getForecast };
+const citySuggestions = async (cityName) => {
+  if (!cityName) return;
+  try {
+    const response = await axios.get(`${BASE_URL}/geo/1.0/direct`, {
+      params: {
+        q: cityName,
+        limit: 5,
+        appid: API_KEY,
+      },
+    });
+    console.log(response);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching city suggestions: ", error);
+  }
+};
+
+export {
+  getCurrentWeather,
+  getForecast,
+  citySuggestions,
+  getCurrentWeatherByCoords,
+};
